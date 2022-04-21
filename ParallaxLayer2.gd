198b57fd98c16b1a2239237abe39b0235116443c
Jannek <jannekNY@gmail.com>
2022-04-21 14:15:22 +0200

    Lavde farten om så det ser bedre ud

--- ParallaxLayer2.gd
+++ ParallaxLayer2.gd
@@ -1,6 +1,8 @@
 extends ParallaxLayer
 
+# dette er til for at lave farten for baggrunden
 export(float) var Sand_Mouinten_Seed = -20
 
+# funktioen som rykker x 
 func _process(delta):
 	self.motion_offset.x += Sand_Mouinten_Seed * delta
--- Parallax_Baackground_Sand.gd
+++ Parallax_Baackground_Sand.gd
@@ -1,6 +1,8 @@
 extends ParallaxLayer
 
+# dette er til for at lave farten for baggrunden
 export(float) var Sand_Background_Seed = -10
 
+# funktioen som rykker x 
 func _process(delta):
 	self.motion_offset.x += Sand_Background_Seed * delta
