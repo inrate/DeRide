de5f923c026a9933347a274e2d0a3a8131dce86a
Jannek <jannekNY@gmail.com>
2022-04-22 10:42:42 +0200

    slettede bare noget ubruglige på player

--- Player.gd
+++ Player.gd
@@ -42,16 +42,4 @@ func _physics_process(delta: float) -> void:
 		_velocity = move_and_slide(_velocity, UP_DIRECTION)
 		
 		
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
