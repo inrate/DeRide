b212424bbbd5a25d62b49bc54219a644f8673216
Jannek <jannekNY@gmail.com>
2022-04-30 21:39:43 +0200

    Har lavet meget på player schild

---
+++ Player_schild.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+export var speed = 500
+export var jump = -350
+export var gravity = 20
+const Up = Vector2.UP
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	#
+	motion.y += gravity
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -speed
+	else:
+		motion.x = 0
+		$AnimationPlayer.play("Idle_player_schild")
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = jump
+	
+	
+	
+	motion = move_and_slide(motion, Up)
+
